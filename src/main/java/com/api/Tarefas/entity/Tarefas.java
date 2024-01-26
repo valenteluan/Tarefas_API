744b344d38125c2ea830926c78112cc115c7a705
Luan Valente <luanbvalente@hotmail.com>
2024-01-26 19:06:48 -0300

    Entity

--- src/main/java/com/api/Tarefas/entity/Tarefas.java
+++ src/main/java/com/api/Tarefas/entity/Tarefas.java
@@ -1,4 +1,68 @@
 package com.api.Tarefas.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tarefas")
 public class Tarefas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+    public Tarefas() {
+    }
+
+    public Tarefas(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
 }
