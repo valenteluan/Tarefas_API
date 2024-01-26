4c7ab76a1f10eca6359285a1e0c277fd800f209c
Luan Valente <luanbvalente@hotmail.com>
2024-01-26 19:21:51 -0300

    Service

--- src/main/java/com/api/Tarefas/repository/TarefasRepository.java
+++ src/main/java/com/api/Tarefas/repository/TarefasRepository.java
@@ -1,6 +1,7 @@
 package com.api.Tarefas.repository;
 
+import com.api.Tarefas.entity.Tarefas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TarefasRepository extends JpaRepository {
+public interface TarefasRepository extends JpaRepository<Tarefas, Long> {
 }
--- src/main/java/com/api/Tarefas/service/TarefasService.java
+++ src/main/java/com/api/Tarefas/service/TarefasService.java
@@ -1,4 +1,37 @@
 package com.api.Tarefas.service;
 
+import com.api.Tarefas.entity.Tarefas;
+import com.api.Tarefas.repository.TarefasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TarefasService {
+
+    @Autowired
+    private TarefasRepository repository;
+
+    public List<Tarefas> create(Tarefas tarefas) {
+        repository.save(tarefas);
+        return list();
+    }
+
+    public List<Tarefas> list() {
+        Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome")).ascending();
+        return repository.findAll(sort);
+    }
+
+    public List<Tarefas> update(Tarefas tarefas) {
+        repository.save(tarefas);
+        return list();
+    }
+
+    public List<Tarefas> delete(Long id) {
+        repository.deleteById(id);
+        return list();
+    }
+
 }
